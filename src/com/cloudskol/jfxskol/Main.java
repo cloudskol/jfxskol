c8cfa40247bffe6b49b663dd9181ce2cf84753ba
Thamizharasu <zarub2k@gmail.com>
2017-10-13 00:36:27 +0530

    Simple change

--- src/com/cloudskol/jfxskol/Main.java
+++ src/com/cloudskol/jfxskol/Main.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        final VBox vBox = new VBox();
+        vBox.getChildren().add(new Label("Hello world."));
+
+        primaryStage.setScene(new Scene(vBox, 500, 300));
         primaryStage.show();
     }
 
